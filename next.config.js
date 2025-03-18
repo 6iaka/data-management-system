86b4f73219428d7c4e15a06559f4f9591aa95991
Lamine Diamoutene <pridila.2006@gmail.com>
2025-03-18 06:23:09 +0200

    make sure all images works no matter the hosting

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "*",
+        protocol: "https",
+      },
+    ],
+  },
   experimental: {
     serverActions: { bodySizeLimit: "700mb" },
   },
